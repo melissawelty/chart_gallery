7ad5399820c976e2e7bdf88e355d631e5a6c44a5
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-04 20:10:26 -0500

    updated solution

--- three_charts.py
+++ three_charts.py
@@ -2,7 +2,12 @@
 
 #
 # CHART 1 (PIE)
-#
+#import motplotlib.pylot as pit
+
+import plotly
+import plotly.graph_objs as go 
+
+
 
 pie_data = [
     {"company": "Company X", "market_share": 0.55},
@@ -13,6 +18,24 @@ pie_data = [
 print("----------------")
 print("GENERATING PIE CHART...")
 print(pie_data) # TODO: create a pie chart based on the pie_data
+
+labels = []
+values = []
+
+for d in pie_data:
+    labels.append(d["company"])
+    values.append(d["market_share"])
+
+
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+fig.show()
+
+
+
+
+
+
+
 
 #
 # CHART 2 (LINE)
@@ -33,20 +56,44 @@ print("----------------")
 print("GENERATING LINE GRAPH...")
 print(line_data) # TODO: create a line graph based on the line_data
 
+
+x = []
+y = []
+
+for d in line_data:
+    x.append(d["date"])
+    y.append(y["stock_price_usd"])
+
+fig = go.Figure(data=go.Scatter(x=x, y=y))
+fig.show()
+
 #
 # CHART 3 (HORIZONTAL BAR)
-#
+# #
 
-bar_data = [
-    {"genre": "Thriller", "viewers": 123456},
-    {"genre": "Mystery", "viewers": 234567},
-    {"genre": "Sci-Fi", "viewers": 987654},
-    {"genre": "Fantasy", "viewers": 876543},
-    {"genre": "Documentary", "viewers": 283105},
-    {"genre": "Action", "viewers": 544099},
-    {"genre": "Romantic Comedy", "viewers": 121212}
-]
+# bar_data = [
+#     {"genre": "Thriller", "viewers": 123456},
+#     {"genre": "Mystery", "viewers": 234567},
+#     {"genre": "Sci-Fi", "viewers": 987654},
+#     {"genre": "Fantasy", "viewers": 876543},
+#     {"genre": "Documentary", "viewers": 283105},
+#     {"genre": "Action", "viewers": 544099},
+#     {"genre": "Romantic Comedy", "viewers": 121212}
+# ]
+
+
 
 print("----------------")
 print("GENERATING BAR CHART...")
 print(bar_data) # TODO: create a horizontal bar chart based on the bar_data
+
+
+x = []
+y = []
+
+for d in bar_data:
+    x.append(d["genre"])
+    y.append(d["viewers"])
+
+fig = go.figure([go.bar(x=x, y=y)])
+fig.show()
